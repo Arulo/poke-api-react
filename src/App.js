38d22bda6b24b5ac0e1f83aeb5398f5e10534f7d
Arulo <arieldanx@gmail.com>
2024-12-04 13:31:19 +0100

    Add first iteration of the pokemon list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Listing from "./screens/listing/Listing";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <main className="content">
-        <p>This is the mock content for the body of the page.</p>
-        <p>Add more text, placeholders, or components here.</p>
+        <h2>List of Pokemon</h2>
       </main>
+      <Listing />
       <Footer />
     </div>
   );
