c28fc6e4b3f31bd4631cf57164d0e00f99e33406
Arulo <arieldanx@gmail.com>
2024-12-05 11:58:53 +0100

    Remove links from header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,6 @@ function Header() {
     <header className="header">
       <img src="/images/logo.png" alt="Pokeball Logo" className="logo" />
       <h1 className="main_heading">PokeAPI Testing Playground</h1>
-      <nav>
-        <ul className="nav_list">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 }
